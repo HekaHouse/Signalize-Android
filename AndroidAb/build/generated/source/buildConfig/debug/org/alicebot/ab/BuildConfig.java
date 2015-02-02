435b4e03aa115275a41f6451555fc721b4ae2a93
Aron Price <aron23@gmail.com>
2015-02-02 17:14:10 -0500

    Mira guidance system expanded to drive main menu as well as note taking process

--- AndroidAb/build/generated/source/buildConfig/debug/org/alicebot/ab/BuildConfig.java
+++ AndroidAb/build/generated/source/buildConfig/debug/org/alicebot/ab/BuildConfig.java
@@ -8,11 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "org.alicebot.ab";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 1;
-  public static final String VERSION_NAME = "1.0";
-  /**
-   * @deprecated Use {@link #APPLICATION_ID}
-   */
-  @Deprecated
-  public static final String PACKAGE_NAME = "org.alicebot.ab";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
 }
